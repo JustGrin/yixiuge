7cf99eee855ee69aabb5eced2558643380a062b0
lidaopeng <876744473@qq.com>
2017-01-18 16:59:54 +0800

    1 汽车查询

--- Application/Conf/data.inc.php
+++ Application/Conf/data.inc.php
@@ -2,8 +2,11 @@
 	return array(
 		/* 数据库设置 */
     'DB_TYPE'               => 'mysql',     // 数据库类型
-
-		'DB_HOST'               => 'localhost', // 服务器地址
+/*		'DB_HOST'               => 'localhost', // 服务器地址
+				'DB_NAME'               => 'yixiuge',          // 数据库名
+				'DB_USER'               => 'yixiuge',      // 用户名fg
+				'DB_PWD'                => 'yxgdir/w',          // 密码*/
+			'DB_HOST'               => 'localhost', // 服务器地址
 				'DB_NAME'               => 'yixiuge',          // 数据库名
 				'DB_USER'               => 'root',      // 用户名fg
 				'DB_PWD'                => 'root',          // 密码
